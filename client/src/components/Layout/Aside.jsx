8a87b833968f1b2d77a9915dc2c4376b295fd6f9
RigobertoVargasGaona <rigovg1054@gmal.com>
2025-09-23 23:00:54 -0500

    Add: Dashboard complete

--- client/src/components/Layout/Aside.jsx
+++ client/src/components/Layout/Aside.jsx
@@ -9,16 +9,16 @@ export const Aside = () => {
       <div className='text-center h-screen w-[15vw] bg-slate-200 font-serif text-2xl'>
         
       <section className='h-[20vh] pt-3  flex  items-center  pl-6 pr-6' >
-         <a href="#" className='flex gap-2  text-slate-500' >
+         <Link to="/dashboard" className='flex gap-2  text-slate-500' >
             <img className=' w-16' src={img.userIcon} alt=''></img>
             <p href='#' className='text-left text-wrap'>Rigoberto Vargas Gaona</p>
-           </a>
+           </Link>
       </section>
    <section className='h-[50vh] pt- flex flex-col'>
   Menú
-    <ul className='text-left  flex flex-col justify-between gap-y-5 mt-4 pl-6 pr-6'>
+    <ul className='text-left  flex flex-col  justify-between gap-y-5 mt-4 pl-6 pr-6 '>
       <li>
-        <Link to ='/inventarios' className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+        <Link to ='/inventarios' className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50' >
            <img className='pl-2' src={img.inventoryIcon} alt=''></img>
            <p  className='block'>Inventarios</p>
@@ -26,15 +26,15 @@ export const Aside = () => {
         </Link>
       </li>
       <li>
-        <Link to='/dashboardIn' className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+        <Link to='/dashboardIn' className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50' >
            <img className='pl-2' src={img.cardIcon} alt=''></img>
-           <p href='#' className='block'>Dashboard</p>
+           <p  className='block'>Dashboard</p>
         </Link>
       </li>
 
       <li>
-        <Link  to ='/users'  className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+        <Link  to ='/users'  className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50'>
             <img className='pl-2' src={img.usersIcon} alt=''></img>
             <p  className='block'>Usuarios</p>
@@ -42,15 +42,15 @@ export const Aside = () => {
       </li>
 
       <li>
-        <Link to='/transformacion'  className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+        <Link to='/transformacion'  className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50'>
           <img className='pl-2' src={img.transformIcon} alt=''></img>
-          <p href='#' className="block" block>Transformación</p>
+          <p  className="block" block>Transformación</p>
         </Link>
        </li>
 
       <li>
-        <Link to='/garantias' className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+        <Link to='/garantias' className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50'>
           <img className='pl-2' src={img.ticketsIcon} alt="" />
           <p>Garantias</p>
@@ -58,7 +58,7 @@ export const Aside = () => {
       </li>
 
       <li>
-        <Link to='/informes' className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+        <Link to='/informes' className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50'>
          <img className='pl-2' src={img.reportsIcon} alt="" /> 
          <p className="block" block>Informes</p>
@@ -71,14 +71,14 @@ export const Aside = () => {
           
           
           <li>
-            <Link to="/ayuda" className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+            <Link to="/ayuda" className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50'>
               <img className='pl-2' src={img.helpIcon} alt=''></img>
               <p  className='block'>Ayuda</p>
             </Link>
           </li>
           <li>
-            <Link  to ="/logIn" className='flex gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
+            <Link  to ="/logIn" className='flex flex-wrap gap-2  shadow-sm text-slate-500 rounded-lg hover:bg-cyan-500 hover:shadow-xl hover:text-white
            hover:shadow-cyan-500/50'>
               <img className='pl-2' src={img.logoutIcon} alt=''></img>
               <p  className='block'>Salir</p>
--- client/src/components/Layout/Main.jsx
+++ client/src/components/Layout/Main.jsx
@@ -1,71 +1,95 @@
 import React from 'react';
-import { DashboardIn } from '../../pages/DashboardIn';
 import { Link } from 'react-router-dom';
+import {img} from '../../assets/img.js';
 
 export const Main = () => {
   return (
     <div className='mx-10  px-32 w-[80vw] h-[93vh]  ' >
-      <div className='p-60 justify-between gap-10 flex flex-wrap '>
+      <div className='p-60 justify-between gap-10 flex flex-wrap text-center '>
         <div>
-        <Link>
+        <Link to ='/inventarios'>
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+              <div className='p-10'>
+                <img className=' w-16' src={img.inventoryIcon} alt=''></img>
+              </div>
             </div>
             <p>Inventarios</p> 
         </Link>
         </div>    
         <div>
-        <Link>
+        <Link to='/dashboardIn'>
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+               <div className='p-10'>
+                  <img className='w-16' src={img.cardIcon} alt=''></img>
+              </div>
             </div>
             <p>Dashboard</p> 
         </Link>
         </div>    
         <div>
-        <Link>
+        <Link to ='/users'>
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+                <div className='p-10'>
+                  <img className='w-16' src={img.usersIcon} alt=''></img>
+                </div>
             </div>
             <p>Usuarios</p> 
         </Link>
         </div>    
         <div>
-        <Link>
+        <Link to='/transformacion' >
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+                <div className='p-10'>
+                  <img className='w-16' src={img.transformIcon} alt=''></img>
+                </div>
             </div>
             <p>Transformación</p> 
         </Link>
         </div>    
         <div>
-        <Link>
+        <Link to='/garantias' >
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+              <div className='p-10'>
+                <img className='w-16' src={img.ticketsIcon} alt="" />
+              </div>
             </div>
             <p>Garantías</p> 
         </Link>
         </div>    
         <div>
-        <Link>
+        <Link to='/informes'>
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+               <div className='p-10'>
+                <img className='w-16' src={img.reportsIcon} alt="" /> 
+              </div>
             </div>
             <p>Informes</p> 
         </Link>
         </div>    
         <div>
-        <Link>
+        <Link to="/ayuda">
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+              <div className='p-10'>
+                <img className='w-16' src={img.helpIcon} alt=''></img>
+              </div>
             </div>
             <p>Ayuda</p> 
         </Link>
         </div>    
         <div>
-        <Link>
+        <Link to ="/logIn">
             <div className=' h-36 w-36 rounded-md shadow-sm shadow-slate-400 hover:bg-cyan-500 hover:shadow-xl hover:text-white
             hover:shadow-cyan-500/50'>
+               <div className='p-10'>
+                <img className='w-16' src={img.logoutIcon} alt=''></img>
+              </div>
             </div>
             <p>Salir</p> 
         </Link>
